bf2222c84c1f02cab97abeb740f47ec29e01dcee
Vallerie Knight <vallerie.knight@gmail.com>
2019-02-18 18:51:42 +0000

    Demo config file

---
+++ config.example.py
@@ -0,0 +1,5 @@
+apikey = "changeme" # tautulli API key
+host = "127.0.0.1:8181" # host and port of tautulli (include base URL for reverse proxy)
+username = "" # if blank, any user will be reported on your profile
+client_id = "changeme" # get this from your discord developer console
+libraries = [] # set this to [] if you want to use any library
